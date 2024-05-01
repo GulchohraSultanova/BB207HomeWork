4906d49819f2cf6d3c8c573e1841a1cf5f4e8378
Gulchohra Sultanova <115097212+GulchohraSultanova@users.noreply.github.com>
2024-05-01 23:38:00 +0400

    Update index.js

--- BB207HomeWork/MovieSite/index.js
+++ BB207HomeWork/MovieSite/index.js
@@ -4,7 +4,7 @@ axios("//api.tvmaze.com/shows")
     // console.log(res.data);
       res.data.forEach(element => {
        list.innerHTML+=`
-       <div class="col-3 col-sm-6 col-md-6 col-lg-6 col-xl-4 col-xxl-3" style="padding:16px; max-height:710px">
+       <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-4 col-xxl-3" style="padding:16px; max-height:710px">
        <div class="card" style="width: 100%;">
        <img class="card-img-top" src="${element.image.medium}" alt="Card image cap">
        <div class="card-body">
